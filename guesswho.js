e9657f8dd7ad04ad4b6208ba050288ed605032de
John Pavek <JohnQPavek@gmail.com>
2018-06-22 19:59:30 -0500

    Adds functional game, needs tuning

--- guesswho.js
+++ guesswho.js
@@ -1,9 +1,25 @@
 function GuessWhoViewModel() {
     var self = this;
 
-    self.onLoad = function() {
-        console.log("Loaded!")
+    self.Images = ko.observableArray();
+
+    self.onLoad = function () {
+        self.Images(config.images.map(function (i) {
+            return new Image(i)
+        }));
     }
 
     self.onLoad();
+}
+
+function Image(img) {
+    var self = this;
+
+    self.src = ko.observable(img.src);
+    self.title = ko.observable(img.title);
+    self.selected = ko.observable(false);
+
+    self.select = function () {
+        self.selected(!self.selected());
+    }
 }
